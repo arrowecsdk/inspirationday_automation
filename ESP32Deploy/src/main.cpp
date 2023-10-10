38b7cabe36241364abdd5eb63ee981a2ea239f40
Jesper Berth <jesper.berth@arrow.com>
2023-10-10 10:35:22 +0200

    Add ESP32Deploy

---
+++ ESP32Deploy/src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <WiFiManager.h>
+
+#define WIFI_AP_NAME "ESP32-Deploy"
+#define WIFI_AP_PASSWORD "ansible64!"
+
+WiFiManager wifiManager;
+
+void setup() {
+
+}
+
+void loop() {
+
+}
